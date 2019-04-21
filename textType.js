9d95ee017f67c79a6d4206ebf99753be1e28ec5f
rismanss <risman.job@gmail.com>
2019-04-21 18:54:29 +0700

    add link and styling

--- textType.js
+++ textType.js
@@ -13,9 +13,10 @@
   textType.defaults = {
     selector : '',
     color: '',
-    data: ['bold','italic','underline','align-left','align-center','align-right'],
+    data: ['align','heading','bold','italic','underline','font','link'],
     font: ["Times New Roman","Arial","Ubuntu","Courier New"],
-    heading: ['H1','H2','H3','H4','H5']
+    heading: ['H1','H2','H3','H4','H5'],
+    align: ['Left','Center','Right','Full']
   }
 
   // constructor of textType
@@ -44,72 +45,149 @@
     this.options.data.map((data) => {
       var li = document.createElement('li');
       var button = document.createElement('button');
-      button.className = 'fa fa-'+data;  
-      
-      ul.appendChild(li);
-      li.appendChild(button);
+      var select = document.createElement('select');
+      switch (data) {
+        case 'align':
+          this.options.align.map((a) => {
+            var opt = document.createElement('option');
+            if (a == 'Center') {
+              opt.innerHTML = '&#xf037; '+a;
+            } else if (a == 'Left') {
+              opt.innerHTML = '&#xf036; '+a;
+            } else if (a == 'Right') {
+              opt.innerHTML = '&#xf038; '+a;
+            } else if (a == 'Full') {
+              opt.innerHTML = '&#xf039; '+a;
+            }
+            opt.value = a;
+            select.value = opt.value;
 
-      // func onclick for edit font
-      button.onclick = function() {
-        // getselection function "bold","italic","underline","left","center","right"
-        // bold, underline, italic
-        document.execCommand(data, false, null);
-        // Set design mode to off
-        document.designMode = "off";
-        if(data == 'align-left') {
-          document.execCommand('JustifyLeft', false, null);
-          document.designMode = "off";
-        } else if (data == 'align-center') {
-          document.execCommand('JustifyCenter', false, null);
-          document.designMode = "off";
-        } else if (data == 'align-right') {
-          document.execCommand('JustifyRight', false, null);
-          document.designMode = "off";
-        }
-      };
-    });
+            select.onclick = function() {
+              editor.focus();
+              document.execCommand('Justify'+select.value, false, null);
+              document.designMode = "off";
+            }
+            
+            select.appendChild(opt);
+          }); 
+          ul.appendChild(li);
+          li.appendChild(select);
+          break;
+        case 'font':
+          this.options.font.map((f) => {
+            var opt = document.createElement('option');
+            select.value = f;
+            opt.style.fontFamily = f;
+            
+            select.onclick = function() {
+              editor.focus();
+              document.execCommand('fontName', false, select.value);
+              document.designMode = "off";
+            }
 
-    // getSelection func font family
-    var uli = document.createElement('li');
-    var selectFont = document.createElement('select');
-    this.options.font.map((f) => {
-      var opt = document.createElement('option');
-      selectFont.value = f;
-      opt.style.fontFamily = f;
-      
-      selectFont.onclick = function() {
-        document.execCommand('fontName', false, selectFont.value);
-        document.designMode = "off";
+            var optText = document.createTextNode(f);
+            opt.appendChild(optText);
+            select.appendChild(opt);
+          });
+          ul.appendChild(li);
+          li.appendChild(select);
+          break;
+        case 'heading':
+          this.options.heading.map((h) => {
+            var opt = document.createElement('option');
+            select.value = h;
+            opt.className = h;
+
+            select.onclick = function() {
+              editor.focus();
+              document.execCommand('formatBlock', false, '<'+select.value+'>');
+              document.designMode = "off";
+            }
+            
+            var optText = document.createTextNode(h);
+            opt.appendChild(optText);
+            select.appendChild(opt);
+          });
+          ul.appendChild(li);
+          li.appendChild(select);
+          break;
+        default: 
+          button.className = 'fa fa-'+data;  
+          
+          ul.appendChild(li);
+          li.appendChild(button);
+
+          // func onclick for edit font
+          button.onclick = function() {
+            if (data == 'link') {
+              // for modal link
+              var modal = document.getElementsByTagName("body")
+                [0].appendChild(document.createElement("div"));
+              modal.id = 'modal';
+
+              var alertObj = modal.appendChild(document.createElement("div"));
+              alertObj.id = "alertBox";
+
+              var h1 = alertObj.appendChild(document.createElement("h1"));
+              h1.appendChild(document.createTextNode("Insert Link"));
+
+              var msg = alertObj.appendChild(document.createElement("input"));
+              msg.className = 'inputLink';
+              msg.placeholder = 'insert link';
+
+              var displayLink = alertObj.appendChild(document.createElement("input"));
+              displayLink.className = 'inputLink';
+              displayLink.placeholder = 'display link';
+              var sel = document.getSelection();
+              displayLink.value = sel;
+
+              var okbtn = alertObj.appendChild(document.createElement("a"));
+              okbtn.id = "okBtn";
+              okbtn.appendChild(document.createTextNode("ok"));
+              okbtn.href = "#";
+
+              var closebtn = alertObj.appendChild(document.createElement("a"));
+              closebtn.id = "closeBtn";
+              closebtn.appendChild(document.createTextNode("close"));
+              closebtn.href = "#";
+
+              editor.focus();
+              const selected = sel.rangeCount > 0 ? sel.getRangeAt(0) : false;
+
+              closebtn.onclick = function() {
+                removeCustomAlert();
+                return false;
+              }
+
+              okbtn.onclick = function() {
+                editor.focus();
+                document.getSelection().removeAllRanges();
+                document.getSelection().addRange(selected);
+                document.execCommand(
+                  'insertHTML', 
+                  false, 
+                  '<a href="' + msg.value + '" target="_blank">' + displayLink.value + '</a>'
+                );
+                removeCustomAlert();
+                return false; 
+              }
+              function removeCustomAlert() {
+                document.getElementsByTagName("body")[0].removeChild(document.getElementById("modal"));
+              }
+            } else {
+              editor.focus();
+              // getselection function "bold","italic","underline"
+              this.classList.toggle('active');
+              // bold, underline, italic
+              document.execCommand(data, false, null);
+              // Set design mode to off
+              document.designMode = "off";
+            }
+          };
       }
-
-      var optText = document.createTextNode(f);
-      opt.appendChild(optText);
-      selectFont.appendChild(opt);
     });
-    ul.appendChild(uli);
-    uli.appendChild(selectFont);
 
-    // getSelection func for font heading
-    var liSize = document.createElement('li');
-    var selectSize = document.createElement('select');
-    this.options.heading.map((h) => {
-      var opt = document.createElement('option');
-      selectSize.value = h;
-      opt.className = h;
-
-      selectSize.onclick = function() {
-        document.execCommand('formatBlock', false, '<'+selectSize.value+'>');
-        document.designMode = "off";
-      }
-      
-      var optText = document.createTextNode(h);
-      opt.appendChild(optText);
-      selectSize.appendChild(opt);
-    });
-    ul.appendChild(liSize);
-    liSize.appendChild(selectSize);
-
-    // footer element
+    // body text element
     var mainBody = document.createElement('div');
     mainBody.className = 'main-body';
     var editor = document.createElement('div');
@@ -124,7 +202,6 @@
     // set color on textType.default
     selector.style.background = this.options.color;
   }
-
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
     // Set for module
